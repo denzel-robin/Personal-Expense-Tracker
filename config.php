a47680c9c50c8e0bc3e8dab57c03d870f48a9441
shadowmonarch13 <144709731+shadowmonarch13@users.noreply.github.com>
2024-10-20 20:38:21 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysqli_connect("localhost","ibexe","673603","dailyexpense");
+$con = mysqli_connect("localhost","root","","dailyexpense");
 if (mysqli_connect_errno())
   {
   echo "Failed to connect to MySQL: " . mysqli_connect_error() ." | Seems like you haven't created the DATABASE with an exact name";
